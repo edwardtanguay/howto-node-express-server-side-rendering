3620db13c0a8a6b2c9346f9cbf23860a269212af
Edward Tanguay (windows) <edward@tanguay.info>
2022-06-14 18:27:17 +0200

    simplified data server

--- dataServer.js
+++ dataServer.js
@@ -1,24 +1,11 @@
 import axios from 'axios';
 import fs from 'fs';
 
-const getNouns = async () => {
-    const nouns = (
-        await axios.get(
-            'https://edwardtanguay.netlify.app/share/germanNouns.json'
-        )
-    ).data;
-    return new Promise((resolve, reject) => {
-        resolve(nouns);
-    });
-};
-
-const getBooks = () => {
-    let rawdata = fs.readFileSync('./data/books.json');
-    return JSON.parse(rawdata);
-};
+const nouns = (await axios.get('https://edwardtanguay.netlify.app/share/germanNouns.json')).data;
+const books = JSON.parse(fs.readFileSync('./data/books.json'));
 
 export const siteData = {
-    siteTitle: "Info Site",
-    nouns: await getNouns(),
-    books: getBooks()
+    siteTitle: 'Info Site',
+    nouns, 
+    books
 };
