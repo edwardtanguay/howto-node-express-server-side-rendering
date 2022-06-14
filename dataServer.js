4a3ff7e5eaf95e44bc7d33a243ce93f4c3affec8
Edward Tanguay (windows) <edward@tanguay.info>
2022-06-15 00:41:30 +0200

    shows number of translations

--- dataServer.js
+++ dataServer.js
@@ -1,13 +1,49 @@
 import axios from 'axios';
 import fs from 'fs';
+import Excel from 'exceljs';
+import * as qmat from './qtools/qmat.js';
 
-const nouns = (await axios.get('https://edwardtanguay.netlify.app/share/germanNouns.json')).data;
+const wb = new Excel.Workbook();
+
+const nouns = (
+    await axios.get(`https://edwardtanguay.netlify.app/share/germanNouns.json`)
+).data;
+
 const books = JSON.parse(fs.readFileSync('./data/books.json'));
-const flashcards = ['nnn', 'ooo'];
+
+const getTranslations = async () => {
+    await wb.xlsx.readFile('./data/googleTranslations.xlsx');
+    const translations = [];
+    // const ws = wb.getWorksheet('Saved translations');
+    const ws = wb.getWorksheet(1);
+    for (let row = 2; row <= 100000; row++) {
+
+        const fromLanguageCell = `A${row}`;
+        const toLanguageCell = `B${row}`;
+        const fromPhraseCell = `C${row}`;
+        const toPhraseCell = `D${row}`;
+
+		const fromLanguage= ws.getCell(fromLanguageCell).value;
+		const toLanguage= ws.getCell(toLanguageCell).value;
+		const fromPhrase= ws.getCell(fromPhraseCell).value;
+		const toPhrase= ws.getCell(toPhraseCell).value;
+        if (fromLanguage === null) {
+            break;
+        } else {
+            translations.push({
+                fromLanguage,
+                toLanguage,
+                fromPhrase,
+                toPhrase
+            });
+        }
+    }
+    return translations;
+};
 
 export const siteData = {
     siteTitle: 'Info Site',
-    nouns, 
+    nouns,
     books,
-    flashcards
+    translations: await getTranslations()
 };
--- site.js
+++ site.js
@@ -1,11 +1,7 @@
-export const site = ({ siteTitle, nouns, books, flashcards }) => {
-	return `
+export const site = ({ siteTitle, nouns, books, translations }) => {
+    return `
 	<h1>${siteTitle}</h1>
-<div>There are ${books.length} books.</div>	
-<div>There are ${nouns.length} nouns.</div>	
-<div>There are ${flashcards.length} flashcards.</div>	
-		
-	<ul>${nouns
-        .map((m) => `<li>${m.article} ${m.singular}</li>`)
-        .join('')}</ul>`;
+	<div>There are ${books.length} books.</div>	
+	<div>There are ${nouns.length} nouns.</div>	
+	<div>There are ${translations.length} translations.</div>`
 };
