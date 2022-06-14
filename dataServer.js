8faf47e39498c862e9614558a7b37c7f3c87c461
Edward Tanguay (windows) <edward@tanguay.info>
2022-06-14 19:44:06 +0200

    showing mock flashcards

--- dataServer.js
+++ dataServer.js
@@ -3,9 +3,11 @@ import fs from 'fs';
 
 const nouns = (await axios.get('https://edwardtanguay.netlify.app/share/germanNouns.json')).data;
 const books = JSON.parse(fs.readFileSync('./data/books.json'));
+const flashcards = ['nnn', 'ooo'];
 
 export const siteData = {
     siteTitle: 'Info Site',
     nouns, 
-    books
+    books,
+    flashcards
 };
--- site.js
+++ site.js
@@ -1,8 +1,9 @@
-export const site = ({ siteTitle, nouns, books }) => {
+export const site = ({ siteTitle, nouns, books, flashcards }) => {
 	return `
 	<h1>${siteTitle}</h1>
 <div>There are ${books.length} books.</div>	
 <div>There are ${nouns.length} nouns.</div>	
+<div>There are ${flashcards.length} flashcards.</div>	
 		
 	<ul>${nouns
         .map((m) => `<li>${m.article} ${m.singular}</li>`)
